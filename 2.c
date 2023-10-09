7460c491fe79da6d00dc75e07fdaabc24bf6975b
eriko666 <147353804+eriko666@users.noreply.github.com>
2023-10-09 08:05:48 +0700

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,12 @@
+//fungsi puts()
+#include <stdio.h>
+
+int main(){
+    putchar('A');
+
+//Fungsi puts() adalah fungsi untuk menampilkan output. Fungsi ini juga terdapat pada library stdio.h.
+//Perbedaanya dengan printf(), fungsi puts() tidak memerlukan format.
+//Fungsi puts() akan selalu membuat baris baru. Jadi tidak perlu menggunakan simbol \n seperti pada printf().
+
+    return 0;
+}
